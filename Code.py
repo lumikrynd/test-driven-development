053eff50e23671b6a6858d5801f789b9ad30c487
Lumikrynd <WhatIsEmail?>
2025-08-09 21:40:45 +0200

    Setup functionality

--- Code.py
+++ Code.py
@@ -2,18 +2,26 @@ class TestCase:
 	def __init__(self, name):
 		self.name = name
 
+	def setUp(self):
+		pass
+
 	def run(self):
 		method = getattr(self, self.name)
+		self.setUp()
 		method()
 
 
 class WasRun(TestCase):
 	def __init__(self, name):
 		self.wasRun = False
+		self.wasSetUp = False
 		TestCase.__init__(self, name)
 
 	def testMethod(self):
 		self.wasRun = True
+
+	def setUp(self):
+		self.wasSetUp = True
 
 
 class TestCaseTest(TestCase):
@@ -23,5 +31,11 @@ class TestCaseTest(TestCase):
 		test.run()
 		assert test.wasRun
 
+	def testSetup(self):
+		test = WasRun("testMethod")
+		test.run()
+		assert test.wasSetUp
+
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetup").run()
