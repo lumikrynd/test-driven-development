0658c23ded03eb962c46a139d7cba38bb449e6f8
Lumikrynd <WhatIsEmail?>
2025-08-09 21:31:56 +0200

    Split out test-case base class

--- Code.py
+++ Code.py
@@ -1,12 +1,27 @@
-class WasRun:
+class TestCase:
+	def __init__(self, name):
+		self.name = name
+
+	def run(self):
+		method = getattr(self, self.name)
+		method()
+
+
+class WasRun(TestCase):
 	def __init__(self, name):
 		self.wasRun = False
+		TestCase.__init__(self, name)
 
 	def testMethod(self):
 		self.wasRun = True
 
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.testMethod()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+	def testRunning(self):
+		test = WasRun("testMethod")
+		assert not test.wasRun
+		test.run()
+		assert test.wasRun
+
+
+TestCaseTest("testRunning").run()
