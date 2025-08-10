786e536c959b03ad89075a33104a67e41a5d181c
Lumikrynd <WhatIsEmail?>
2025-08-10 08:35:02 +0200

    Add testsuite and fixed some errors
    - Found out python class variable basically acts as statics... fixed.
    - Add "add" function to TestResult (so they can be combined)
    - Add TestSuite class
    - Rewrite how current tests are run to use TestSuite

--- Code.py
+++ Code.py
@@ -9,6 +9,10 @@ class TestResult:
 	def testFailed(self):
 		self.errorCount += 1
 
+	def addResult(self, other):
+		self.runCount += other.runCount
+		self.errorCount += other.errorCount
+
 	def summary(self):
 		return "%d run, %d failed" % (self.runCount, self.errorCount)
 
@@ -40,8 +44,26 @@ class TestCase:
 		return result
 
 
+class TestSuite:
+	def __init__(self):
+		self.tests = []
+
+	def add(self, test):
+		self.tests.append(test)
+
+	def run(self):
+		result = TestResult()
+		for test in self.tests:
+			testResult = test.run()
+			result.addResult(testResult)
+
+		return result
+
+
 class WasRun(TestCase):
-	log = ""
+	def __init__(self, name):
+		self.log = ""
+		super().__init__(name)
 
 	def setUp(self):
 		self.log += "setup "
@@ -72,9 +94,28 @@ class TestCaseTest(TestCase):
 		result = test.run()
 		assert result.summary() == "1 run, 1 failed"
 
+	def testSuite(self):
+		suite = TestSuite()
+		suite.add(WasRun("testMethod"))
+		suite.add(WasRun("testBrokenMethod"))
+		result = suite.run()
+		assert result.summary() == "2 run, 1 failed"
 
-assert TestCaseTest("testTemplateMethod").run().errorCount == 0
-assert TestCaseTest("testResult").run().errorCount == 0
-assert TestCaseTest("testFailedResult").run().errorCount == 0
+
+def runTestcaseTest(name):
+	suite.add(TestCaseTest(name))
+
+
+suite = TestSuite()
+
+runTestcaseTest("testTemplateMethod")
+runTestcaseTest("testResult")
+runTestcaseTest("testFailedResult")
+runTestcaseTest("testSuite")
+
+finalResult = suite.run()
+print(finalResult.summary())
+
+assert finalResult.errorCount == 0
 
 print("\033[92mGreen\033[0m")
